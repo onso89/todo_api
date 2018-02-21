a264be1c10fdfdf86d6b84bd556b78045b7b485c
Alonso Ahmetaj <alonsoahmetaj@gmail.com>
2018-02-21 16:37:45 +0000

    add createtodo logic and refactoring

--- public/app.js
+++ public/app.js
@@ -2,13 +2,39 @@
 
 $(document).ready(function(){
    $.getJSON("/api/todos")
-   .then(addTodos);
+   .then(showTodos);
+   
+   $('#todoInput').keypress(function(event){
+     if(event.which == 13){
+      createTodo();   
+     }
+   });
 });
 
-function addTodos(todos) {
+function showTodos(todos) {
     // add todos to page
     todos.forEach(function(todo){
-      var newTodo = $('<li class="task">' + todo.name + '</li>');
+        showTodo(todo);
+    });
+}
+
+function showTodo(todo){
+     var newTodo = $('<li class="task">' + todo.name + '</li>');
+     if(todo.completed){
+        newTodo.addClass('done');  
+      }
       $(".list").append(newTodo);
+}
+
+function createTodo(){
+    //send request to creat new todo
+    var userInput = $('#todoInput').val();
+    $.post("/api/todos", {name: userInput})
+    .then(function(todo){
+       $('todoInput').val('');
+       showTodo(todo);
+    })
+    .catch(function(error){
+       console.log(error); 
     });
 }
