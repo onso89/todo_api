1b9ad9baa5ed6b162ea7b5dc19b98d4f891884a4
Alonso Ahmetaj <alonsoahmetaj@gmail.com>
2018-02-21 15:40:17 +0000

    add ajax logic to manipulate dom

--- public/app.js
+++ public/app.js
@@ -1,1 +1,14 @@
-alert("Hello from Public dir");
+/*global $ */
+
+$(document).ready(function(){
+   $.getJSON("/api/todos")
+   .then(addTodos);
+});
+
+function addTodos(todos) {
+    // add todos to page
+    todos.forEach(function(todo){
+      var newTodo = $('<li class="task">' + todo.name + '</li>');
+      $(".list").append(newTodo);
+    });
+}
